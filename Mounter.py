63ed9f4d4b3c95c833e90903f2a80ad73d49ca5f
Arrogant Rabbit <chipmunk@arrogantrabbit.com>
2021-10-20 20:30:28 -0700

    More aggressive cache persistence

--- Mounter.py
+++ Mounter.py
@@ -15,24 +15,56 @@ rclone_binary = "/usr/local/bin/rclone"
 logging_level = logging.INFO
 log_folder = os.path.join(user_home, "Library", "Logs", "Mounter")
 
-# These settings are indispensable for mount stability on macOS
+# We assume the data will only be changed through this remote, 
+# and not by any other means, including web interface or another rclone instance,
+# we are going to effectively disable remote refresh
 uber_important_rclone_options = [
-    "--drive-acknowledge-abuse",  # to avoid I/O errors when
+    #
+    # to avoid I/O errors when file is considered "harmful" by Google 
+    "--drive-acknowledge-abuse",
+    #
+    # Full cache mode for best compatibility
     "--vfs-cache-mode",
     "full",
+    #
+    # 1TB max total size cached
     "--vfs-cache-max-size",
     "1024G",
-    "--daemon-timeout",  # To avoid spurious disconnects
+    #
+    # Cache files forever
+    "--vfs-cache-max-age",
+    "1000h",
+    #
+    # Check cache for stale objects every so often. Default is 1m, 5 min is good enough,
+    # we mainly want to remove files from cache once the size exceeds the max-size above
+    "--vfs-cache-poll-interval",
+    "5m",
+    #
+    # time the kernel caches the attributes for. Reduces roundtrips to kernel,
+    # no risk of corruption since files don't change externally. Otherwise -- remove it!
+    "--attr-timeout",
+    "60s",
+    #
+    # Cache directories forever.
+    # We can always send "SIGHUP" to force refresh directory caches
+    "--dir-cache-time",
+    "1000h",
+    #
+    # Don't poll for changes on remote
+    "--poll-interval",
+    "0",
+    #
+    # For how long kernel should wait before giving up. Imperative for mount stabilty
+    "--daemon-timeout",
     "599s",
+    #
+    # run as a daemon
     "--daemon",
-    "--dir-cache-time",  # To avoid frequent directory re-fetch
-    "48h",
 ]
 
 # Executes the short running command line utility and logs outputs
 def run_helper(shortname, args):
     logging.info("Launching {}: {}".format(shortname, " ".join(args)))
-
     p = subprocess.run(args, capture_output=True, text=True)
     if p.stdout:
         logging.info("{}: {}".format(shortname, p.stdout))
@@ -163,7 +195,9 @@ else:
             elif show_log_caption in action:
                 run_helper("open", ["open", make_rclone_log_path(item)])
             else:
-                logging.error("Action {} is unrecognized. Doing nothing.".format(action))
+                logging.error(
+                    "Action {} is unrecognized. Doing nothing.".format(action)
+                )
     if show_mounter_log_caption in action:
         run_helper("open", ["open", os.path.join(log_folder, "Mounter.log")])
     elif flush_directory_caches in action:
